95a5693961bed080855d441cbf6367386bcd2976
Tatyana Prokopenko <tanya021179@gmail.com>
2022-11-01 11:22:03 +0100

    add folder java

---
+++ java/src/main/java/com/epam/training/student_Tetiana_Prokopenko/App.java
@@ -0,0 +1,13 @@
+package com.epam.training.student_Tetiana_Prokopenko;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
